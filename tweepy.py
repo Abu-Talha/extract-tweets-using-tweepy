bc3554a71acbacec21929ec2b92e3d278456f122
Abu Talha <53007985+Abu-Talha@users.noreply.github.com>
2020-05-19 21:11:40 +0530

    Create tweepy.py

---
+++ tweepy.py
@@ -0,0 +1,18 @@
+#pass twitter credentials to tweepy
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth,wait_on_rate_limit=True)
+# Open/Create a file to append data
+csvFile = open('output.csv', 'a')
+#Use csv Writer
+csvWriter = csv.writer(csvFile)
+
+for tweet in tweepy.Cursor(api.search,q="#covid19",count=100,
+                           lang="en", region="India",
+                           since="2020-03-24",max=("2020-03-26")).items():
+    print (tweet.created_at, tweet.text)
+    csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
